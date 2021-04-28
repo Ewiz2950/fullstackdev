639b0b4c9f8e8880e614cdc74156ce1747cd7382
Min <limminxuan14@gmail.com>
2021-04-28 23:36:21 +0800

    added bootstrap and bootstrap icons

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, "node_modules", 
+        "bootstrap-icons", "font")));
 
 
 app.use('/', indexRouter);
