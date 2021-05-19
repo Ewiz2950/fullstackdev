7c2844ae2b498dc1c312f5134efedae1d0206168
Min <limminxuan14@gmail.com>
2021-05-19 13:56:48 +0800

    templates: index, product grid, product page

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,21 @@ router.get('/', function (req, res, next) {
     res.send(templates.main(data));
 });
 
+router.get('/search', function (req, res, next) {
+    var data = {
+        body: templates.productGrid(data),
+    };
+
+    res.send(templates.main(data));
+});
+
+router.get('/product', function (req, res, next) {
+    var data = {
+        body: templates.product(data),
+    };
+
+    res.send(templates.main(data));
+});
+
 
 module.exports = router;
