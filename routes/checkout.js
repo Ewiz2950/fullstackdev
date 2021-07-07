1bdc80c536631e86f17761ad09c704eb462d4ee8
KaiJun-GP <kaijun.chia@gmail.com]>
2021-07-07 10:37:45 +0800

    help

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var templates = require('../dist/views/templates.js');
+var db = require('../config/connection.js');
 
 // Checkout Page
 router.get('/', function (req, res, next) {
     var data = {
         body: templates.checkout(),
     };
+    const userDetails = req.body;
+    var sql = 'INSERT INTO users SET ?';
+      db.query(sql, userDetails,function (err, data) { 
+  });
 
     res.send(templates.main(data));
 });
