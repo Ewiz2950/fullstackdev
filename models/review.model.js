c70ac77bab435b67ff4a745596b8628c7c0afe7f
Junhao2950 <koojh2950@gmail.com>
2021-07-12 15:28:43 +0800

    review

---
+++ models/review.model.js
@@ -0,0 +1,102 @@
+const connection = require('../config/connection.js')
+
+// constructor
+const Review = function(review) {
+    this.review_id = review.review_id
+    this.product_id = review.product_id
+    this.review_star = review.review_star
+    this.review_text = review.review_text
+};
+
+Review.create = (review, result) => {
+    connection.query("INSERT INTO review SET ?", review, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+      console.log("created review: ", { review_id: review.review_id, product_id: review.product_id, review_star: review.review_star, review_text: review.review_text });
+      result(null, res);
+    });
+  };
+  
+Review.findById = (productId, result) => {
+    connection.query(`SELECT * FROM review WHERE product_id = ${productId}`, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+  
+      if (res.length) {
+        console.log("found product: ", res[0]);
+        result(null, res[0]);
+        return;
+      }
+  
+      // not found Product with the id
+      result({ status: "not_found" }, null);
+    });
+  };
+  
+// Review.getAll = result => {
+//     connection.query("SELECT * FROM customers", (err, res) => {
+//       if (err) {
+//         console.log("error: ", err);
+//         result(null, err);
+//         return;
+//       }
+  
+//       console.log("customers: ", res);
+//       result(null, res);
+//     });
+//   };
+  
+Review.updateById = (id, product, result) => {
+    connection.query(
+      `UPDATE product SET name = ?, description = ?, price = ?, category = ?, 
+       subCategory = ?, promotion = ? WHERE id = ?`,
+      [product.name, product.description, product.price, product.category, 
+       product.subCategory, product.promotion],
+      (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(null, err);
+          return;
+        }
+  
+        if (res.affectedRows == 0) {
+          // not found Product with the id
+          result({ "status": "not_found" }, null);
+          return;
+        }
+  
+        console.log("updated product: ", { id: id, product });
+        result(null, { id: id, product });
+      }
+    );
+  };
+  
+Review.setListing = (id, product, listing, result) => {
+      connection.query(
+        "UPDATE product SET listing = ? WHERE product_id = ?", listing,
+        (err, res) => {
+          if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+          }
+    
+          if (res.affectedRows == 0) {
+            // not found Product with the id
+            result({ status: "not_found" }, null);
+            return;
+          }
+    
+          console.log("updated product: ", { id: id, product });
+          result(null, { id: id, product });
+        }
+      );
+    };
+  
+module.exports = Review;
